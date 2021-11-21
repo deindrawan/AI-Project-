d092db402e0124ba7de4009d3a225a548d44b38c
I Gede Indrawan <92201527+deindrawan@users.noreply.github.com>
2021-11-21 14:19:41 +0700

    Add files via upload

---
+++ luas-segitiga.py
@@ -0,0 +1,8 @@
+print("Program Menentukan Luas Segitiga")
+print("\n")
+a = float(input("\nMasukan Alas  : "))
+t = float(input("Masukan Tinggi: "))
+
+luas = 0.5*a*t
+
+print("\nLuas Segitiga  = %0.2f" % luas)
---
+++ volume-balok.py
@@ -0,0 +1,9 @@
+print("Program Menentukan Volume Balok")
+print("\n")
+p = float(input("\nMasukan Panjang  : "))
+l = float(input("Masukan Lebar: "))
+t = float(input("Masukan Tinggi: "))
+
+v = p*l*t
+
+print("\nVolume Balok  = %0.2f" % v)
